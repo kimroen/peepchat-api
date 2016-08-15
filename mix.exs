011f42ac99ace8354a4769ac08c108c0e0b5e166
Kim Røen <kim@kimroen.com>
2016-08-15 22:14:10 +0200

    Add comeonin to handle password hashing with bcrypt

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Peepchat.Mixfile do
   def application do
     [mod: {Peepchat, []},
      applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -34,6 +34,7 @@ defmodule Peepchat.Mixfile do
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:guardian, "~> 0.12.0"},
+     {:comeonin, "~> 2.5"},
      {:postgrex, ">= 0.0.0"},
      {:gettext, "~> 0.11"},
      {:cors_plug, "~> 1.1"},
--- web/models/user.ex
+++ web/models/user.ex
@@ -5,6 +5,10 @@ defmodule Peepchat.User do
     field :email, :string
     field :password_hash, :string
 
+    # Two virtual fields for password confirmation
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
     timestamps()
   end
 
@@ -13,7 +17,12 @@ defmodule Peepchat.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
+    |> cast(params, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> hash_password
+  defp hash_password(%{valid?: false} = changeset), do: changeset
+  defp hash_password(%{valid?: true} = changeset) do
+    hashedpw = Comeonin.Bcrypt.hashpwsalt(Ecto.Changeset.get_field(changeset, :password))
+    Ecto.Changeset.put_change(changeset, :password_hash, hashedpw)
   end
 end
