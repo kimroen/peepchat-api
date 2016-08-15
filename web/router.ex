9dace8f71089072731d189c54e7c223a6e75012a
Kim Røen <kim@kimroen.com>
2016-08-15 23:29:05 +0200

    Create registration  endpoint

--- web/router.ex
+++ web/router.ex
@@ -8,6 +8,9 @@ defmodule Peepchat.Router do
   scope "/api", Peepchat do
     pipe_through :api
 
+    # Registration
+    post "/register", RegistrationController, :create
+
     resources "/session", SessionController, only: [:index]
   end
 end
