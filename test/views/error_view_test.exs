f4e3af804035b0d6152d07f922c005478008cbfb
Kim Røen <kim@kimroen.com>
2016-08-16 10:42:51 +0200

    Add a test for the 401 error

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -4,6 +4,11 @@ defmodule Peepchat.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  test "renders 401.json" do
+    assert render(Peepchat.ErrorView, "401.json", []) ==
+           %{errors: [%{code: 401, title: "Unauthorized"}]}
+  end
+
   test "renders 404.json" do
     assert render(Peepchat.ErrorView, "404.json", []) ==
            %{errors: [%{code: 404, title: "Resource Not Found"}]}
