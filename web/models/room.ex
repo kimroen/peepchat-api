ce186654063ecd2c7c4eee433c7d9960a50804a6
Kim Røen <kim@kimroen.com>
2016-08-24 22:46:01 +0200

    Generate room

---
+++ web/models/room.ex
@@ -0,0 +1,19 @@
+defmodule Peepchat.Room do
+  use Peepchat.Web, :model
+
+  schema "rooms" do
+    field :name, :string
+    belongs_to :owner, Peepchat.Owner
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name])
+    |> validate_required([:name])
+  end
+end
